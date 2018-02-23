8cf4d7306e8f243bd4316c63c35eb222097c88a7
delaj161 <john.x.dela.-nd@disney.com>
2018-02-23 12:01:32 +0800

    added lottery contract

--- contract/lottery.sol
+++ contract/lottery.sol
@@ -1,5 +1,33 @@
-pragma solidity 0.4.19;
+pragma solidity ^0.4.17;
+
 
 contract Lottery {
+    address public manager;
+    address[] public players;
+    
+    function Lottery() public {
+        manager = msg.sender;
+    }
+    
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+    
+    function random() private view returns (uint) {
+       return uint(keccak256(block.difficulty, now, players));
+    }
+    
+    function pickWinner() public isOwner {
+        require(players.length > 1);
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+    
+    modifier isOwner() {
+        require(msg.sender == manager);
+        _;
+    }
     
 }
